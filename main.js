cfa2df057e002909a9feb3134f97cf4ebfa8cd26
youness52 <anayouness52@gmail.com>
2025-10-23 08:33:31 +0100

    hgf

---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  // Load your website (HTTP or HTTPS)
+  win.loadURL('http://192.168.15.53/admin_login.php');
+
+  // Optional: Open DevTools for debugging
+  // win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
